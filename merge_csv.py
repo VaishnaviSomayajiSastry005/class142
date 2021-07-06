b968f820da4bbaabe97a6200e7ebbafd3ffe3c10
VaishnaviSomayajiSastry <58731609+VaishnaviSomayajiSastry005@users.noreply.github.com>
2021-07-06 20:05:26 +0530

    Add files via upload

---
+++ merge_csv.py
@@ -0,0 +1,27 @@
+import csv
+with open("movies.csv") as f:
+    reader=csv.reader(f)
+    data=list (reader)
+    all_movies=data[1:]
+    headers=data[0]
+headers.append("poster_link")
+ 
+with open("final.csv","a+")as f:
+    csvWriter=csv.writer(f)
+    csvWriter.writerow(headers)
+
+with open("movies_links.csv")as f:
+    reader=csv.reader(f)
+    data=list(reader)
+    all_movies_links=data[1:]
+
+for movie_item in all_movies:
+    poster_found=any(movie_item[8] in movie_link_items for movie_link_items in all_movies_links)
+    if poster_found:
+        for movie_link_item in all_movies_links:
+            if movies_item[8]==movie_link_item[0]:
+                movies_item.append(movie_link_item[1])
+                if len(movie_item)==28:
+                    with open("final.csv","a+")as f:
+                        csvWriter=csv.writer(f)
+                        csvWriter.writerow(movie_item)
